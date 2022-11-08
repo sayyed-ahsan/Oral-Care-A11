bcf7792a106ec87c486f8f41260b49f59618afc1
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-11-09 01:54:52 +0600

    5th

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from './Carousel';
 import SectionA from './SectionA';
+import ServicesSection from './ServicesSection';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
             <SectionA></SectionA>
+            <ServicesSection></ServicesSection>
         </div>
     );
 };
---
+++ src/Pages/Home/ServicesSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ServicesSection = () => {
+    return (
+        <div>
+
+            <p className='text-center text-orange-400 text-6xl'>Services</p>
+
+        </div>
+    );
+};
+
+export default ServicesSection;
