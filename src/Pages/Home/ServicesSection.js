bd4239c94cefd4454855871def27bca2f70499c5
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-11-09 05:04:53 +0600

    card done

--- src/Pages/Home/ServicesSection.js
+++ src/Pages/Home/ServicesSection.js
@@ -5,6 +5,17 @@ const ServicesSection = () => {
         <div>
 
             <p className='text-center text-orange-400 text-6xl'>Services</p>
+            {/* 
+            <div className="card w-90 bg-base-100 shadow-xl image-full">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{service.sName}</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem sit nesciunt voluptatem at, reicijsdl;</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-outline btn-success">Show Detail</button>
+                                </div>
+                            </div>
+                        </div> */}
 
         </div>
     );
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -6,17 +6,20 @@ const Services = () => {
     const img = allService[1].image;
     console.log(img);
     return (
-        <div className='grid grid-cols-2 gap-6 justify-items-center my-8'>
+        <div className='grid grid-cols-3 gap-2 justify-items-center mx-1 my-8'>
             {
                 allService.map(service =>
                     <div key={service._id}>
-                        <div className="card w-96 bg-base-100 shadow-xl image-full">
-                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">{service.sName}</h2>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem sit nesciunt voluptatem at, reicijsdl;</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary">Buy Now</button>
+
+                        <div className="card w-90 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-outline btn-success">Show Detail</button>
                                 </div>
                             </div>
                         </div>
@@ -26,5 +29,16 @@ const Services = () => {
         </div>
     );
 };
+
+// <div className="card w-90 bg-base-100 shadow-xl image-full">
+//     <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+//     <div className="card-body">
+//         <h2 className="card-title">{service.sName}</h2>
+//         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem sit nesciunt voluptatem at, reicijsdl;</p>
+//         <div className="card-actions justify-end">
+//             <button className="btn btn-outline btn-success">Show Detail</button>
+//         </div>
+//     </div>
+// </div>
 
 export default Services;
