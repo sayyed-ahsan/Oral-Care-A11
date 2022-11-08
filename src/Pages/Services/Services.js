f36fe2e446bf06e2c7799b1d34ad438971c88af4
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-11-09 03:37:52 +0600

    services

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+// import React from 'react';
+// import { useLoaderData } from 'react-router-dom';
 
-const Services = () => {
-    const allService = useLoaderData();
-    return (
-        <div>
-            Sercives{allService.length}
-            {
-                allService.map(service =>
-                    <div key={service._id}>
-                        <h1>{service.sName}</h1>
-                    </div>
-                )
-            }
-        </div>
-    );
-};
+// const Services = () => {
+//     const allService = useLoaderData();
+//     return (
+//         <div>
+//             Sercives{allService.length}
+//             {
+//                 allService.map(service =>
+//                     <div key={service._id}>
+//                         <h1>{service.sName}</h1>
+//                     </div>
+//                 )
+//             }
+//         </div>
+//     );
+// };
 
-export default Services;
+// export default Services;
